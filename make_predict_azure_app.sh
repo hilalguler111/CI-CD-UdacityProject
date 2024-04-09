4827a838ea6c66fba937d8ce3b3896de70b3047b
hilalguler111 <79762223+hilalguler111@users.noreply.github.com>
2024-04-09 18:00:27 +0200

    Update make_predict_azure_app.sh

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://flaskaktoprak.azurewebsites.net:$PORT/predict 
+     -X POST https://webapphilaL.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
